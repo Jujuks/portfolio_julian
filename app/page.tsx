e595b5ed1193fc306c51031c8e5137fe34eead7b
Jujuks <160888397+Jujuks@users.noreply.github.com>
2026-04-21 11:42:15 -0500

    feat: assemble main landing page sections

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import Navbar from "./_components/Navbar";
+import Hero from "./_components/Hero";
+import Experience from "./_components/Experience";
+import ProjectGrid from "./_components/ProjectGrid";
+import Testimonials from "./_components/Testimonials";
+import SkillSection from "./_components/SkillSection";
+import ExtraExperience from "./_components/ExtraExperience";
+import Contact from "./_components/Contact";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        <Hero />
+        <Experience />
+        <ProjectGrid />
+        <Testimonials />
+        <SkillSection />
+        <ExtraExperience />
+        <Contact />
+      </main>
+    </div>
+  );
+}
